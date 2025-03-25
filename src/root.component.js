9c4c816ec2adbed51c8e50a15e5d6ae1e76db52e
Sushruth Sastry <shastry.sushruth@gmail.com>
2025-03-25 16:13:11 -0700

    chore: cleaning up unnecessary files

--- src/root.component.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export default function Root() {
-    return (_jsx("section", { children: "My amazing component from app2 is mounted! Hello world" }));
-}
